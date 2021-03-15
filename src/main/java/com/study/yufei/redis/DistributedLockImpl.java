d7d52a1f48da4f524b15255724dd88d50da26444
elvis <yufei2020@gmail.com>
2021-03-15 17:46:06 +0800

    1. 分布式锁: 引入计数器

--- src/main/java/com/study/yufei/redis/DistributedLockImpl.java
+++ src/main/java/com/study/yufei/redis/DistributedLockImpl.java
@@ -8,9 +8,13 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 1. 如果其他地方没有调用tryLock方法，而是直接调用了releaseLock方法，传的key是一样的，那就会出现问题：把别人的锁释放掉了
- * 2. 还有一种场景就是在提交订单的接口方法中，调用了服务A，服务A调用了服务B，而服务B的方法中存在对同一个商品的加锁和解锁操作。
+ * 2. 锁重入：还有一种场景就是在提交订单的接口方法中，调用了服务A，服务A调用了服务B，而服务B的方法中存在对同一个商品的加锁和解锁操作。
  * 所以，服务B成功设置锁标志位后，提交订单的接口方法继续执行时，也不能成功设置锁标志位了。
  * 也就是说，目前实现的分布式锁没有可重入性。
+ * 3. 计数器：假设我们提交订单的方法中，首先使用RedisLock接口对代码块添加了分布式锁，在加锁后的代码中调用了服务A，而服务A中也存在调用RedisLock接口的加锁和解锁操作。而多次调用RedisLock接口的加锁操作时，只要之前的锁没有失效，则会直接返回true，表示成功获取锁。
+ * 也就是说，无论调用加锁操作多少次，最终只会成功加锁一次。
+ * 而执行完服务A中的逻辑后，在服务A中调用RedisLock接口的解锁方法，
+ * 此时，会将当前线程所有的加锁操作获得的锁全部释放掉。
  *
  * @author yufei.wang
  * @date 2021/03/15 17:13
@@ -26,9 +30,11 @@ public class DistributedLockImpl implements DistributedLock {
 
     private ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
+    private ThreadLocal<Integer> threadLocalCounter = new ThreadLocal<>();
+
     @Override
     public Boolean tryLock(String key, long timeout, TimeUnit unit) {
-        Boolean isLocked = false;
+        Boolean isLocked;
         // 当前线程已获取锁，再次调用直接返回true，锁可重入
         if (threadLocal.get() == null) {
             String uuid = UUID.randomUUID().toString();
@@ -36,6 +42,11 @@ public class DistributedLockImpl implements DistributedLock {
             isLocked = stringRedisTemplate.opsForValue().setIfAbsent(key, uuid, timeout, unit);
         } else {
             isLocked = true;
+        }
+        if(isLocked){
+            // 加锁成功，计数器+1
+            Integer count = threadLocalCounter.get() == null ? 0 : threadLocalCounter.get();
+            threadLocalCounter.set(count++);
         }
 
         return isLocked;
@@ -45,7 +56,11 @@ public class DistributedLockImpl implements DistributedLock {
     public void releaseLock(String key) {
         //当前线程中绑定的uuid与Redis中的uuid相同时，再执行删除锁的操作
         if (threadLocal.get().equals(stringRedisTemplate.opsForValue().get(key))) {
-            stringRedisTemplate.delete(key);
+            Integer count = threadLocalCounter.get();
+            // 线程计数器不存在或是为0，可以释放锁
+            if(null == count || --count <= 0){
+                stringRedisTemplate.delete(key);
+            }
         }
     }
 }
