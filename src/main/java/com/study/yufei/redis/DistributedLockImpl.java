7156aa7d70b6cf7bac8a5a97bfbf8b61c61c1076
elvis <yufei2020@gmail.com>
2021-03-15 17:36:55 +0800

    1. 分布式锁: 重入性

--- src/main/java/com/study/yufei/redis/DistributedLockImpl.java
+++ src/main/java/com/study/yufei/redis/DistributedLockImpl.java
@@ -8,6 +8,10 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 1. 如果其他地方没有调用tryLock方法，而是直接调用了releaseLock方法，传的key是一样的，那就会出现问题：把别人的锁释放掉了
+ * 2. 还有一种场景就是在提交订单的接口方法中，调用了服务A，服务A调用了服务B，而服务B的方法中存在对同一个商品的加锁和解锁操作。
+ * 所以，服务B成功设置锁标志位后，提交订单的接口方法继续执行时，也不能成功设置锁标志位了。
+ * 也就是说，目前实现的分布式锁没有可重入性。
+ *
  * @author yufei.wang
  * @date 2021/03/15 17:13
  */
@@ -24,15 +28,23 @@ public class DistributedLockImpl implements DistributedLock {
 
     @Override
     public Boolean tryLock(String key, long timeout, TimeUnit unit) {
-        String uuid = UUID.randomUUID().toString();
-        threadLocal.set(uuid);
-        return stringRedisTemplate.opsForValue().setIfAbsent(key, uuid, timeout, unit);
+        Boolean isLocked = false;
+        // 当前线程已获取锁，再次调用直接返回true，锁可重入
+        if (threadLocal.get() == null) {
+            String uuid = UUID.randomUUID().toString();
+            threadLocal.set(uuid);
+            isLocked = stringRedisTemplate.opsForValue().setIfAbsent(key, uuid, timeout, unit);
+        } else {
+            isLocked = true;
+        }
+
+        return isLocked;
     }
 
     @Override
     public void releaseLock(String key) {
         //当前线程中绑定的uuid与Redis中的uuid相同时，再执行删除锁的操作
-        if(threadLocal.get().equals(stringRedisTemplate.opsForValue().get(key))){
+        if (threadLocal.get().equals(stringRedisTemplate.opsForValue().get(key))) {
             stringRedisTemplate.delete(key);
         }
     }
