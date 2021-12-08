13b5797d11135184277f0189c1c5a4e0b30e6e98
elvis <yufei2020@gmail.com>
2021-12-08 21:15:48 +0800

    1. Selector 选择器demo

---
+++ src/main/java/com/study/yufei/nio/NioDiscardClient.java
@@ -0,0 +1,50 @@
+package com.study.yufei.nio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * @date 2021/12/8 4:48 PM
+ */
+public class NioDiscardClient {
+
+    private final static Logger log = LoggerFactory.getLogger(NioDiscardClient.class);
+
+    public static void startClient() throws IOException {
+
+        InetSocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 18889);
+        // SocketChannel客户端连接服务器
+        SocketChannel socketchannel = SocketChannel.open(socketAddress);
+        socketchannel.configureBlocking(false);
+
+        while (!socketchannel.finishConnect()){
+
+        }
+        log.info("客户端连接成功！");
+        System.out.println("请输入要发送的内容：");
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String next = scanner.next();
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            buffer.put((new Date() + " >>" + next).getBytes());
+            buffer.flip();
+            socketchannel.write(buffer);
+            buffer.clear();
+        }
+
+        //socketchannel.shutdownOutput();
+        //socketchannel.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        startClient();
+    }
+}
---
+++ src/main/java/com/study/yufei/nio/NioDiscardServer.java
@@ -0,0 +1,78 @@
+package com.study.yufei.nio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+/**
+ * Discard服务器：仅仅读取客户端通道的输入数据，读取完成后直接关闭客户端通道，并且读取到的数据直接抛弃掉
+ *
+ * @date 2021/12/8 11:32 AM
+ */
+public class NioDiscardServer {
+
+    private static final Logger log = LoggerFactory.getLogger(NioDiscardServer.class);
+
+    public static void startServer() throws IOException {
+
+        // 获取选择器
+        Selector selector = Selector.open();
+
+        // 获取通道
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 设置为非阻塞
+        serverSocketChannel.configureBlocking(false);
+        // 绑定端口
+        serverSocketChannel.bind(new InetSocketAddress(18889));
+        log.info("服务器启动成功！");
+
+        // 将通道 接收新连接 IO事件，注册到选择器上
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // 轮询IO就绪事件（选择键集合）
+        while (selector.select() > 0) {
+            // 获取选择键集合
+            Iterator<SelectionKey> selectionKeys = selector.selectedKeys().iterator();
+
+            while (selectionKeys.hasNext()) {
+                // 获取单个的选择键，并处理
+                SelectionKey selectionKey = selectionKeys.next();
+                // 判断key是具体的什么事件
+                if (selectionKey.isAcceptable()) {
+
+                    // 若选择键的IO事件是 连接就绪 事件，就获取客户端连接
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+
+                    // 将新连接切换为非阻塞模式
+                    socketChannel.configureBlocking(false);
+                    // 将新连接的通道的可读事件，注册到选择器上
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (selectionKey.isReadable()) {
+                    // 若选择键的IO事件是可读事件，读取数据
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                    // 读取数据，然后丢弃
+                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                    int length = 0;
+                    if ((length = socketChannel.read(buffer)) > 0) {
+                        buffer.flip();
+                        log.info(new String(buffer.array(), 0, length));
+                        buffer.clear();
+                    }
+                    socketChannel.close();
+                }
+                selectionKeys.remove();
+            }
+        }
+        serverSocketChannel.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        startServer();
+    }
+}
